405ebe370310f80905dd64b1e7f50fdd0428c36c
Yonatan Laurence <laurence0521@gmail.com>
2024-01-07 18:17:28 +0200

    Nasa clean up

--- nasa.rb
+++ nasa.rb
@@ -7,10 +7,11 @@ ENV_VARS = Dotenv.load(__dir__ + '/.env')
 
 class Nasa < Sinatra::Base
   API_ENDPOINT = "https://api.nasa.gov/planetary/apod"
+  
   get '/nasa' do
-    random = HTTP.get(API_ENDPOINT, params: {
+    picture_of_the_day = HTTP.get(API_ENDPOINT, params: {
       api_key: ENV_VARS['NASA_API_KEY']
     })
-    erb :nasa, locals: random.parse
+    erb :nasa, locals: picture_of_the_day.parse
   end
 end
