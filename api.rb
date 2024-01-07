de809ed428fccbf1375e2011f466507d7fd2b69e
Yonatan Laurence <laurence0521@gmail.com>
2024-01-07 16:47:35 +0200

    Add API POST with query params and JSON payload

--- api.rb
+++ api.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'sinatra/base'
 
+DATA_IN_MEMORY = {}
+
 class Api < Sinatra::Base
   before %r{/api\/?.*} do
     content_type 'application/json'
@@ -8,5 +10,30 @@ class Api < Sinatra::Base
 
   get '/api' do
     { status: :ok }.to_json
+  end
+
+  post '/api/create_with_params' do
+    key = params[:name]
+    save_data(key, params[:data])
+    print_data(key)
+  end
+
+  post '/api/create_with_json_body' do
+    payload = JSON.parse request.body.read
+    key = payload["name"]
+    save_data(key, payload["data"])
+    print_data(key)
+  end
+
+  private
+
+  def save_data(key, value)
+    logger.info "Adding #{value} to #{key}..."
+    DATA_IN_MEMORY[key] = [] if DATA_IN_MEMORY[key].nil?
+    DATA_IN_MEMORY[key] << value
+  end
+
+  def print_data(key)
+    { key => DATA_IN_MEMORY[key] }.to_json
   end
 end
