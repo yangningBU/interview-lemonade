ce8f5ab314709521972d5d8d6edafc5b054cc0b8
Yonatan Laurence <laurence0521@gmail.com>
2024-01-08 12:41:52 +0200

    Convert HTML response to JSON for Api::V1 route

--- api/v1.rb
+++ api/v1.rb
@@ -1,12 +1,17 @@
+require 'json'
 require 'sinatra/base'
 require 'sinatra/namespace'
 
 class Api::V1 < Sinatra::Base
   register Sinatra::Namespace
 
+  before do
+    content_type 'application/json'
+  end
+
   namespace '/api/v1' do
     get '/hello' do
-      'world'
+      { "hello" => "world" }.to_json
     end
   end
 end
